af8a491fdb2fc4897c5c564b666245eeae9257b4
Carlito Algarin <carlito1113@yahoo.com>
2021-05-01 17:18:11 -0400

    updated the api call so that the temperatures provided are imperial and not kelvin

--- fetch.js
+++ fetch.js
@@ -3,7 +3,7 @@ class Fetch {
     const myKey = '023779ffdc30d5ad709a6318ac74d817';
 
     const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${myKey}`
+      `https://api.openweathermap.org/data/2.5/weather?q=${input}&units=imperial&appid=${myKey}`
     );
 
     const data = await response.json();
